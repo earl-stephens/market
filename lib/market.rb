ced784d3d4420ddbb34f06f180c67259dd141ae9
earl-stephens <stephensearl@hotmail.com>
2019-01-08 09:46:48 -0700

    finished methods and tests for iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -1,4 +1,30 @@
 class Market
+  attr_reader :name,
+              :vendors
+
   def initialize(name)
+    @name = name
+    @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    vendor_item_array = []
+    @vendors.each do |vendor|
+      if vendor.inventory.include?(item)
+        vendor_item_array << vendor
+      end
+    end
+    vendor_item_array
+  end
+
 end
