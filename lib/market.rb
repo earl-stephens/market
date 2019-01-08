bdc21b6dc7b716886d2b9deffe6053862810b4d3
earl-stephens <stephensearl@hotmail.com>
2019-01-08 10:23:48 -0700

    added test_it_can_determine_if_there_are_enough_items and sell method

--- lib/market.rb
+++ lib/market.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Market
   attr_reader :name,
-              :vendors
+              :vendors,
+              :total_inventory
 
   def initialize(name)
     @name = name
@@ -37,13 +39,23 @@ class Market
   end
 
   def total_inventory
-    total_inventory = Hash.new(0)
+    @total_inventory = Hash.new(0)
     @vendors.each do |vendor|
       vendor.inventory.each do |item, quantity|
-        total_inventory[item] += quantity
+        @total_inventory[item] += quantity
       end
     end
-    total_inventory
+    @total_inventory
+  end
+
+  def sell(item, quantity)
+    # binding.pry
+    if quantity > @total_inventory[item]
+      return false
+    else
+      # sell_item(item,quantity)
+      return true
+    end
   end
 
 end
