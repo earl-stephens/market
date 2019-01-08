30c5563e13350dbcc341883b55ec0f63a4b3506a
earl-stephens <stephensearl@hotmail.com>
2019-01-08 09:09:38 -0700

    made vendor class

---
+++ test/vendor_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+  end
+
+end
